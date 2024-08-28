861e4fd724d54f03be8332b45cbc342f645f29c9
artcreatezart <abbie@Abbies-MBP.hub>
2024-08-29 10:32:32 +1200

    set up basic skeleton and content into footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>This Website was made for educational purposes.<br></br><br></br>Design's used in this website are from the students and credited in each post</p>
+    </footer>
+  )
+}
+
+export default Footer
