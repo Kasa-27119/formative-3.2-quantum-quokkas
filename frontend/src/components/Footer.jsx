3bd95910d3ea4a7cf54947d0da12f30dddd00cbd
artcreatezart <abbie@Abbies-MBP.hub>
2024-08-29 10:34:52 +1200

    added comments and breaks into footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,21 @@
+// Imports
 import React from 'react'
 
+/** CONTENT IN FOOTER.JSX
+ * Imports (2)
+ * Footer html (10-13)
+ */
+
 const Footer = () => {
+
+  // Footer HTML Content
   return (
     <footer>
-      <p>This Website was made for educational purposes.<br></br><br></br>Design's used in this website are from the students and credited in each post</p>
+      <p>This Website was made for educational purposes.
+        <br></br>
+        <br></br>
+        Design's used in this website are from the students and credited in each post
+      </p>
     </footer>
   )
 }
