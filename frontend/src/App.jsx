2ac6ab1e76784997b63a0ff1070a9d8b0843df11
artcreatezart <abbie@Abbies-MBP.hub>
2024-08-29 11:55:39 +1200

    added some of linking into app.jsx including home and some components ran into an error so i will be going back to previous files to fix it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,41 @@
+// Imports
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import './App.css'
+
+// Page Imports
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
+import SingleProject from './pages/SingleProject'
+
+// component Imports
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+/** CONTENTS OF APP.JSX
+ * Imports (2 -13) Page Imports (6-9) component imports (12-13)
+ * App.jsx htmml (23 -)
+ * 
+ */
 
 const App = () => {
 
   return (
-    <>
-    test
-    </>
+    /**COMMENTS / UPDATES /TODO
+     * Not fully done html fucntionality for app.jsx 
+     * as we will need to add in user reltated functionailty once its added to previous files
+     *  */ 
+    <div className='app'>
+      <BrowserRouter>
+        <Navbar/>
+        <div className='pages'>
+          <Routes>
+            <Home/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+    </div>
   )
 }
 
