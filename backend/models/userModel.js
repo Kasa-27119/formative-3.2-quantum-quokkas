0c9268ff53f5e6bf7a39300648720d82d1efefe6
Mere Frances <meredithflss@gmail.com>
2024-08-30 11:36:49 +1200

    created signupUser and hashing passwords, worked in userModel and userController

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 // mongoose
 const mongoose = require('mongoose');
+// bCrypt
+const bcrypt = require('bcrypt');
 // schema setup
 const Schema = mongoose.Schema;
 
@@ -15,5 +17,24 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+    const exists = await this.findOne({email})
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    // generating salt with 10 characters
+    const salt = await bcrypt.genSalt(10);
+    // hash the password and salt combined
+    const hash = await bcrypt.hash(password, salt);
+
+    // set password to the hash value when user is created
+    const user = await this.create({email, password: hash});
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
