43f9e8f7630e78a6a328c59f4f866d21eb85e0a8
Mere Frances <meredithflss@gmail.com>
2024-09-02 09:50:29 +1200

    added useEffect inside AuthContext frontend

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -20,7 +20,7 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {user: null})
 
-    useInsertionEffect(() => {
+    useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'))
         if(user) {
             dispatch({type: 'LOGIN', payload: user})
