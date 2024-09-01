52a14389737fa78783b71e5a24bf2b20d1115d4a
artcreatezart <abbie@Abbies-MacBook-Pro.local>
2024-09-02 09:56:58 +1200

    Created get single project function, edited files projects.js lines 10 and 17  and projectController.js lines 52-71 and 76

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -7,12 +7,14 @@ const router = express.Router();
 // controller import
 const {
     getProjects,
+    getProject,
     createProject,
     deleteProject
 } = require('../controllers/projectController');
 
 // http request
 router.get('/', getProjects);
+router.get('/:id', getProject)
 router.post('/', createProject);
 router.delete('/:id', deleteProject);
 
