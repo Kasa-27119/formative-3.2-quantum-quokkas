5335b3d1cc60bab82ae4c2ddd472e66d9213cbe6
artcreatezart <abbie@Abbies-MBP.hub>
2024-09-03 21:41:39 +1200

    added layout of SingleProject.js installed react bootstrapicons in the frontend, edited files App.css and singleProject.jsx ive edited sections variables - h1 - singleproject page

--- frontend/src/pages/SingleProject.jsx
+++ frontend/src/pages/SingleProject.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ArrowLeftShort } from 'react-bootstrap-icons'
 
 const SingleProject = () => {
   return (
@@ -8,27 +9,29 @@ const SingleProject = () => {
 
             {/* top container */}
             <div id='single-top-container'>
-                <span>&#2190</span>
+                <ArrowLeftShort className='back-arrow'/>
                 <h1>Project Name</h1>
             </div>
-        </div>
 
-        {/* content container */}
-        <div className='single-content-container'>
-            {/* project image */}
-            <div className='placeholder-img'></div>
+            {/* content container */}
+            <div className='single-content-container'>
+                {/* project image */}
+                <div className='img-container'></div>
 
-            {/* project text */}
-            <div className='single-text-container'>
-                <h2>Project Author</h2>
-                <h3>Created At:</h3>
+                {/* project text */}
+                <div className='single-text-container'>
+                    <h2>Project Author</h2>
+                    <h3>Created At:</h3>
 
-                <div className='description-container'>
-                    <p className='body-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat turpis pulvinar tristique fermentum. Vestibulum vestibulum, nisl quis imperdiet interdum, augue purus tristique erat, eu tristique nunc risus sed dui. Maecenas sollicitudin efficitur lorem, eget pretium leo lobortis id. Pellentesque nulla nibh, aliquam vitae consectetur non, mattis et massa. Morbi vitae elit gravida, ultrices dolor et, rhoncus turpis.</p>
-                    <p className='body-text'>Quisque porta hendrerit massa, nec gravida mauris vulputate eget. Sed vel enim sit amet erat facilisis sagittis. Sed at aliquam quam. Etiam in sem a orci commodo dignissim. Nullam interdum, lacus eu pulvinar finibus, elit odio placerat eros, in facilisis mauris odio vel nunc. Nulla eget faucibus ex. Nunc feugiat sit amet libero quis mollis.</p>
+                    <div className='description-container'>
+                        <p className='body-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat turpis pulvinar tristique fermentum. Vestibulum vestibulum, nisl quis imperdiet interdum, augue purus tristique erat, eu tristique nunc risus sed dui. Maecenas sollicitudin efficitur lorem, eget pretium leo lobortis id. Pellentesque nulla nibh, aliquam vitae consectetur non, mattis et massa. Morbi vitae elit gravida, ultrices dolor et, rhoncus turpis.</p>
+                        <p className='body-text'>Quisque porta hendrerit massa, nec gravida mauris vulputate eget. Sed vel enim sit amet erat facilisis sagittis. Sed at aliquam quam. Etiam in sem a orci commodo dignissim. Nullam interdum, lacus eu pulvinar finibus, elit odio placerat eros, in facilisis mauris odio vel nunc. Nulla eget faucibus ex. Nunc feugiat sit amet libero quis mollis.</p>
+                    </div>
                 </div>
             </div>
         </div>
+
+        
     </>
   )
 }
